dead90bab1eaa5a5d29953676a168409171523f5
MVanza <68096976+MVanza@users.noreply.github.com>
2020-12-10 16:09:13 +0300

    1 пункт

--- ex1.py
+++ ex1.py
@@ -1,3 +1,15 @@
 import numpy as np
+import pandas as pd
 
-print('hi')
+N, M = tuple(map(int, input().split()))
+df = pd.DataFrame()
+for i in range(N):
+    d = np.array(list(map(int, input().split())))
+    df[f'string{i}'] = d.transpose()
+summ = 0
+for i in range(N):
+    d = df[df[f'string{i}'] < -5]
+    d1 = d.loc[:, f'string{i}'].values
+    summ += len(d1)
+
+print(summ)
