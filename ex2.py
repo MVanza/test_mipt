476b04378a492dc888fea2c788e529662982937c
MVanza <68096976+MVanza@users.noreply.github.com>
2020-12-10 16:49:54 +0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,4 +1,14 @@
 import numpy as np
 
-print('hi')
-print('fuck')
+with open(input()) as f:
+    n1 = np.array(list(map(float, f.read().split())))
+
+with open(input()) as f:
+    n2 = np.array(list(map(float, f.read().split())))
+
+n3 = np.array(list(map(float, input().split())))
+n1 = np.reshape(n1, (len(n2), len(n3)))
+
+A2 = np.linalg.matrix_power(n1, 2)
+
+print(A2.dot(n3).dot(n2))
