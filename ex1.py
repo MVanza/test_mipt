6473eda3040d10a33cebb38e7ff658fc6b39e39e
MVanza <68096976+MVanza@users.noreply.github.com>
2020-12-10 18:55:54 +0300

    Новое решение через нампай

--- ex1.py
+++ ex1.py
@@ -1,25 +1,21 @@
 import numpy as np
-import pandas as pd
+
 
 N, M = tuple(map(int, input().split()))
-df = pd.DataFrame()
+c = np.zeros(N, M)
+
 for i in range(N):
-    d = np.array(list(map(int, input().split())))
-    df[f'string{i}'] = d.transpose()
-# 1
+    c[i] += (list(map(int, input().split())))
 S = 0
 V = 0
-maximum = 0
-for i in range(N):
-    # 1
-    ds = df[df[f'string{i}'] < -5]
-    S += len(ds.loc[:, f'string{i}'].values)
-    # 2
-    dv = df[df[f'string{i}'] < 0]
-    V += sum(dv.loc[:, f'string{i}'].values)
 
-# 3
-m = df.max().values
+for i in range(N):
+    for j in c[i]:
+        if j < 0:
+            V += j
+            if j < -5:
+                S += 1
+
 print(S)
-print(abs(V))
-print(max(m))
+print(abs(int(V)))
+print(int(c.max()))
